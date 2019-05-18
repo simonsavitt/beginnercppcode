b3c32889e85c3ef3c07d16b41d808b942b5ffc30
Simon Savitt <simon.savitt@gmail.com>
2019-05-18 07:56:46 -0400

    Add files via upload

---
+++ oddnumsbtw1and100.cpp
@@ -0,0 +1,9 @@
+// print odd numbers between 1-100
+#include <iostream>
+
+int main()
+{
+    for(int i = 1; i <=100; i++) {
+        if(i%2==1) std::cout<<i<<std::endl;
+    }
+}
---
+++ sumoddnumsbtw1and100.cpp
@@ -0,0 +1,11 @@
+// sum odd numbers between 1-100
+#include <iostream>
+
+int main()
+{
+    int sum = 0;
+    for(int i = 1; i <=100; i++) {
+        if(i%2==1) sum+=i; 
+    }
+    std::cout<<sum<<std::endl;
+}
