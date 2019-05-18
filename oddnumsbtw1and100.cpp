45d86c00f435ce84a20a85adeab3b4d19ae5862a
Simon Savitt <simon.savitt@gmail.com>
2019-05-18 08:08:50 -0400

    Add files via upload

--- oddnumsbtw1and100.cpp
+++ oddnumsbtw1and100.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    for(int i = 1; i <=100; i++) {
+    for(int i=1; i <=100; i++) {
         if(i%2==1) std::cout<<i<<std::endl;
     }
 
--- sumoddnumsbtw1and100.cpp
+++ sumoddnumsbtw1and100.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     int sum = 0;
-    for(int i = 1; i <=100; i++) {
+    for(int i=1; i <=100; i++) {
         if(i%2==1) sum+=i; 
     }
     std::cout<<sum<<std::endl;
