96ce5bfa8eed14ede71a43b7dc2afa08584fc7fe
Simon Savitt <simon.savitt@gmail.com>
2019-05-18 08:16:24 -0400

    Add files via upload

---
+++ sum_x_to_y.cpp
@@ -0,0 +1,22 @@
+// sum numberes from x to y, with y>x
+#include <iostream>
+
+int main()
+{
+    int x, y, sum = 0;
+    std::cout<<"enter x: ";
+    std::cin>>x;
+    std::cout<<"enter y: ";
+    std::cin>>y;
+    while (y<=x) {
+        std::cout<<"y<=x, please re-enter"<<std::endl;
+        std::cout<<"enter y: ";
+        std::cin>>y;
+    }
+    for(int i = x; i<=y; i++) {
+        sum+=i;
+    }
+    std::cout<<sum;
+    
+    return 0;
+}
